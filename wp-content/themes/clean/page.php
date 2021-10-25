570b8abd0fe78ba27dc5c1270ef1cd0bc23749f4
Danil <danilbe168@gmail.com>
2021-10-25 18:05:05 +0300

    working on tags in product cards

--- wp-content/themes/clean/page.php
+++ wp-content/themes/clean/page.php
@@ -14,27 +14,55 @@
 
 get_header();
 ?>
+<main>
+    <div class="wrapper">
 
-	<main id="primary" class="site-main">
+        <?php
+        $args = array(
+            'post_type'      => 'product',
+            'posts_per_page' => 10,
+
+        );
+
+        $loop = new WP_Query( $args );
+
+        while ( $loop->have_posts() ) : $loop->the_post();?>
+            <?php global $product;?>
+
+            <div class="product-card">
+                <div class="top-info">
+                    <p class ="id-shift" style="background-color: <?php echo get_field('current_work_status')?>"><i class="<?php echo get_field('shifts')?>"></i>#<?php echo $product->get_sku(); ?>
+                    </p>
+                </div>
+                <div class="image-place">
+                    <img src="<?php echo wp_get_attachment_url( $product->get_image_id()); ?>" alt="Product image">
+                </div>
+                <div class="title-skills">
+                    <H3><?php echo get_the_title() ?></H3>
+                    <p><?php
+                        $terms = get_terms( 'product_tag');
+                        $term_array = array();
+                        if ( ! empty( $terms ) && ! is_wp_error( $terms ) ){
+                            foreach ( $terms as $term ) {
+                                echo $term->name;
+                            }
+                        }
+                        ?></p>
+                </div>
 
 
+            </div>
+        <?php endwhile;
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+        wp_reset_query();
+        ?>
 
-			get_template_part( 'template-parts/content', 'page' );
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+    </div>
 
-		endwhile; // End of the loop.
-		?>
+</main>
 
-	</main><!-- #main -->
 
 <?php
-get_sidebar();
+
 get_footer();
