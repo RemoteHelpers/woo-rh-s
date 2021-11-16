ee3b3d979e30a83e41998af33d0cfdd8b87db0a7
limbitskiy <gagarinbrood@gmail.com>
2021-11-16 17:12:36 +0200

    remove fixed height from employee cards

--- wp-content/themes/clean/js/index.js
+++ wp-content/themes/clean/js/index.js
@@ -29,37 +29,10 @@ jQuery(document).ready(function(){
 });
 
 /* AUTOMATIC CV-TITLE FONT SCALER */
-(function( $ ){
-
-    $.fn.fitText = function( kompressor, options ) {
-
-        // Setup options
-        var compressor = kompressor || 1,
-            settings = $.extend({
-                'minFontSize' : Number.NEGATIVE_INFINITY,
-                'maxFontSize' : Number.POSITIVE_INFINITY
-            }, options);
-
-        return this.each(function(){
-
-            // Store the object
-            var $this = $(this);
-
-            // Resizer() resizes items based on the object width divided by the compressor * 10
-            var resizer = function () {
-                $this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
-            };
-
-            // Call once to set.
-            resizer();
-
-            // Call on resize. Opera debounces their resize by default.
-            $(window).on('resize.fittext orientationchange.fittext', resizer);
-
-        });
-
-    };
-
-})( jQuery );
-
-jQuery('.single-product-sidebar .product_title').fitText(.5);
+const title = document.querySelector('.single-product-sidebar .product_title')
+const titleText = title.innerText
+const titleLength = titleText.length
+const k = 270
+console.log(titleText.length)
+title.style.fontSize = k / titleLength + 'px'
+console.log( k / titleLength )
