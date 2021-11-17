feb27ea6bd01551b2672b1f66b09d1fab5af8bcf
limbitskiy <gagarinbrood@gmail.com>
2021-11-17 11:55:59 +0200

    add final styles to header

--- wp-content/themes/clean/js/index.js
+++ wp-content/themes/clean/js/index.js
@@ -29,10 +29,56 @@ jQuery(document).ready(function(){
 });
 
 /* AUTOMATIC CV-TITLE FONT SCALER */
-const title = document.querySelector('.single-product-sidebar .product_title')
-const titleText = title.innerText
-const titleLength = titleText.length
-const k = 270
-console.log(titleText.length)
-title.style.fontSize = k / titleLength + 'px'
-console.log( k / titleLength )
+// const title = document.querySelector('.single-product-sidebar .product_title')
+// const titleText = title.innerText
+// const titleLength = titleText.length
+// const k = 270
+// console.log(titleText.length)
+// title.style.fontSize = k / titleLength + 'px'
+// console.log( k / titleLength )
+
+/*global jQuery */
+/*!
+* FitText.js 1.2
+*
+* Copyright 2011, Dave Rupert http://daverupert.com
+* Released under the WTFPL license
+* http://sam.zoy.org/wtfpl/
+*
+* Date: Thu May 05 14:23:00 2011 -0600
+*/
+
+(function( $ ){
+
+    jQuery("#responsive_headline").fitText();
+
+    $.fn.fitText = function( kompressor, options ) {
+
+        // Setup options
+        var compressor = kompressor || 1,
+            settings = $.extend({
+                'minFontSize' : Number.NEGATIVE_INFINITY,
+                'maxFontSize' : Number.POSITIVE_INFINITY
+            }, options);
+
+        return this.each(function(){
+
+            // Store the object
+            var $this = $(this);
+
+            // Resizer() resizes items based on the object width divided by the compressor * 10
+            var resizer = function () {
+                $this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
+            };
+
+            // Call once to set.
+            resizer();
+
+            // Call on resize. Opera debounces their resize by default.
+            $(window).on('resize.fittext orientationchange.fittext', resizer);
+
+        });
+
+    };
+
+})( jQuery );
