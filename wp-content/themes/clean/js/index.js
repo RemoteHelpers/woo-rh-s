2bc405e09e131f5badffe9ecc39c840eab3c96ee
limbitskiy <gagarinbrood@gmail.com>
2021-11-17 15:43:58 +0200

    make some adaptive edits

--- wp-content/themes/clean/js/index.js
+++ wp-content/themes/clean/js/index.js
@@ -28,57 +28,26 @@ jQuery(document).ready(function(){
 });
 });
 
-/* AUTOMATIC CV-TITLE FONT SCALER */
-// const title = document.querySelector('.single-product-sidebar .product_title')
-// const titleText = title.innerText
-// const titleLength = titleText.length
-// const k = 270
-// console.log(titleText.length)
-// title.style.fontSize = k / titleLength + 'px'
-// console.log( k / titleLength )
 
-/*global jQuery */
-/*!
-* FitText.js 1.2
-*
-* Copyright 2011, Dave Rupert http://daverupert.com
-* Released under the WTFPL license
-* http://sam.zoy.org/wtfpl/
-*
-* Date: Thu May 05 14:23:00 2011 -0600
-*/
+function autoFontScale() // auto scale title font based on letter quantity
+{
+    const max = 32
+    const min = 18
+    const k = 270
+    const title = document.querySelector('.single-product-sidebar .product_title')
 
-(function( $ ){
+    const titleText = title.innerText
+    const titleLength = titleText.length
+    const newSize = k / titleLength
 
-    jQuery("#responsive_headline").fitText();
+    if (newSize > max) {
+        title.style.fontSize = max  + 'px'
+    } else if (newSize < min) {
+        title.style.fontSize = min  + 'px'
+    } else {
+        title.style.fontSize = newSize  + 'px'
+    }
+}
 
-    $.fn.fitText = function( kompressor, options ) {
+autoFontScale();
 
-        // Setup options
-        var compressor = kompressor || 1,
-            settings = $.extend({
-                'minFontSize' : Number.NEGATIVE_INFINITY,
-                'maxFontSize' : Number.POSITIVE_INFINITY
-            }, options);
-
-        return this.each(function(){
-
-            // Store the object
-            var $this = $(this);
-
-            // Resizer() resizes items based on the object width divided by the compressor * 10
-            var resizer = function () {
-                $this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
-            };
-
-            // Call once to set.
-            resizer();
-
-            // Call on resize. Opera debounces their resize by default.
-            $(window).on('resize.fittext orientationchange.fittext', resizer);
-
-        });
-
-    };
-
-})( jQuery );
