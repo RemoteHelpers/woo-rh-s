8a0d6afca764a077ceef1994abc642dfb8fc1d0e
limbitskiy <gagarinbrood@gmail.com>
2021-11-23 18:20:17 +0200

    make changes to header, change logo, add second logo to clean theme, add svg support

--- wp-content/themes/clean/header.php
+++ wp-content/themes/clean/header.php
@@ -28,30 +28,54 @@
         <header id="masthead" class="site-header">
             <div class="site-branding">
                 <div class="site-logo">
-                    <?php the_custom_logo();
-                    ?>
+                    <?php
+                    //                    if ( function_exists( 'the_custom_logo' ) ) {
+                    //                        the_custom_logo();
+                    //                    }
+                    //                    $logo_img = '';
+                    //                    if( $custom_logo_id = get_theme_mod('dark_logo') ){
+                    //                        $logo_img = wp_get_attachment_image( $custom_logo_id, 'full', false, array(
+                    //                            'class'    => 'dark_logo',
+                    //                            'itemprop' => 'logo',
+                    //                        ) );
+                    //                    }
+                    //                    echo $logo_img;
+                    $custom_logo_id = get_theme_mod('dark_logo');
+                    //                    $logo_src = wp_get_attachment_image_src($custom_logo_id, 'full');
+
+
+                    if (is_front_page()) { ?>
+                        <img src="<?php echo $custom_logo_id ?>">
+                    <?php } else {
+                        the_custom_logo();
+                    } ?>
                 </div>
 
-<!--                --><?php
-//                the_custom_logo();
-//                if (is_front_page() && is_home()) :
-//                    ?>
-<!--                    <h1 class="site-title"><a href="--><?php //echo esc_url(home_url('/')); ?><!--"-->
-<!--                                              rel="home">--><?php //bloginfo('name'); ?><!--</a></h1>-->
-<!--                --><?php
-//                else :
-//                    ?>
-<!--                    <p class="site-title"><a href="--><?php //echo esc_url(home_url('/')); ?><!--"-->
-<!--                                             rel="home">--><?php //bloginfo('name'); ?><!--</a></p>-->
-<!--                --><?php
-//                endif;
-//                $clean_description = get_bloginfo('description', 'display');
-//                if ($clean_description || is_customize_preview()) :
-//                    ?>
-<!--                    <p class="site-description">-->
-<!--                        --><?php //echo $clean_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-//                        ?><!--</p>-->
-<!--                --><?php //endif; ?>
+                <!--                --><?php
+                //                the_custom_logo();
+                //                if (is_front_page() && is_home()) :
+                //                    ?>
+                <!--                    <h1 class="site-title"><a href="-->
+                <?php //echo esc_url(home_url('/')); ?><!--"-->
+                <!--                                              rel="home">-->
+                <?php //bloginfo('name'); ?><!--</a></h1>-->
+                <!--                --><?php
+                //                else :
+                //                    ?>
+                <!--                    <p class="site-title"><a href="-->
+                <?php //echo esc_url(home_url('/')); ?><!--"-->
+                <!--                                             rel="home">-->
+                <?php //bloginfo('name'); ?><!--</a></p>-->
+                <!--                --><?php
+                //                endif;
+                //                $clean_description = get_bloginfo('description', 'display');
+                //                if ($clean_description || is_customize_preview()) :
+                //                    ?>
+                <!--                    <p class="site-description">-->
+                <!--                        -->
+                <?php //echo $clean_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+                //                        ?><!--</p>-->
+                <!--                --><?php //endif; ?>
             </div><!-- .site-branding -->
 
             <nav id="site-navigation" class="main-navigation">
